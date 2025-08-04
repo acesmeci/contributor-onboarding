b9445ec7bd5554a5e7265d046569d3147f51e03b
thayashi-01 <213783448+thayashi-01@users.noreply.github.com>
2025-08-04 09:27:36 +0000

    just a test

---
+++ tests/multiply.py
@@ -0,0 +1,16 @@
+def multiply(a, b) -> float:
+    '''
+    This function returns the product of two numbers
+
+    Args:
+    a: float the first number
+    b: float the second number
+
+    Returns:
+    float
+    '''
+    return a * b
+
+
+#Expected behavior: Returns the float product of two numbers.
+#Actual behavior: Accepts non-floats and may return unexpected types.t
